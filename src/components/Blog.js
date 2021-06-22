096d20504471fb32452ea0017a143e4338a61197
StefanoBranz <stefano.branz@gmail.com>
2021-06-22 11:45:48 -0300

    blog component CSS done for @media 320x568

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -14,35 +14,39 @@ export default function Blog() {
           tinyone
         </h1>
         <section className="header-menu">
-          <h4>Features</h4>
-          <h4>Support</h4>
-          <h4>Blog</h4>
+          <h4 className="header-menu-h4">Features</h4>
+          <h4 className="header-menu-h4">Support</h4>
+          <h4 className="header-menu-h4">Blog</h4>
         </section>
       </header>
       <main>
         <section>
-          <article>
-            <h2>Inspire your inspiration</h2>
-            <h5>Simple to use for your app, products showcase and your inspiration</h5>
-            <p>
+          <article className="blog-article-container">
+            <h2 className="blog-article-title">Inspire your inspiration</h2>
+            <h5 className="blog-article-subtitle">Simple to use for your app, products showcase and your inspiration</h5>
+            <p className= "blog-article-paragraph">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Praesent vitae eros eget tellus tristique bibendum.
               Donec rutrum sed sem quis venenatis. Proin viverra risus
               a eros volupat tempor. In quis arcu et eros porta lobortis sit
             </p>
-            <section>
-              <img src={ AppleBlackIcon } alt="Apple icon" />
-              <img src={ AndroidBlackIcon } alt="Android icon" />
-              <img src={ WindowsBlackIcon } alt="Windows icon" />
+            <section className="blog-article-icons--container">
+              <img className="blog-article-icons--img" src={ AppleBlackIcon } alt="Apple icon" />
+              <img className="blog-article-icons--img" src={ AndroidBlackIcon } alt="Android icon" />
+              <img className="blog-article-icons--img" src={ WindowsBlackIcon } alt="Windows icon" />
             </section>
           </article>
-          <section>
-            <img src={ TabletPhoto } alt="Tablet with app logo and name" />
+          <section className="blog-tablet-container">
+            <img
+              src={ TabletPhoto }
+              alt="Tablet with app logo and name"
+              className="blog-tablet-img"
+            />
           </section>
           <section className="blog-carousel-container" >
-            <span className="dot" />
-            <span className="dot" />
-            <span className="dot" />
+            <span className="blog-carousel-dot" />
+            <span className="blog-carousel-dot" />
+            <span className="blog-carousel-dot" />
           </section>
         </section>
       </main>
