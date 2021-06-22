354f5266cc5f5e8b6995aeac45aad363379d39cd
StefanoBranz <stefano.branz@gmail.com>
2021-06-21 23:18:40 -0300

    structured support component

--- src/components/Features.js
+++ src/components/Features.js
@@ -4,16 +4,16 @@ import FeatureCards from './FeaturesCards';
 export default function Features() {
   return (
     <div className="features-background">
-      <section className="features-header">
+      <header className="features-header">
         <h1>Tinyone features</h1>
         <h3>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
           Praesent vitae eros eget tellus tristique bibendum. Donec rutrum sed sem quis venenatis.
         </h3>
-      </section>
-      <section>
+      </header>
+      <main>
         <FeatureCards />
-      </section>
+      </main>
     </div>
   )
 }
--- src/components/Support.js
+++ src/components/Support.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import FacebookIcon from '../icons/facebook-icon.png';
+import TwitterIcon from '../icons/twitter-icon.png';
+import GoogleIcon from '../icons/google-icon.png';
+import PinterestIcon from '../icons/pinterest-icon.png';
+import SupportFooter from './SupportFooter';
 
 export default function Support() {
   return (
     <div className="support-background">
-      <h1>Support</h1>
+      <header className="support-header-container">
+        <h1 className="support-header-title">Keep in touch with us</h1>
+        <h3 className="support-header-h3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Repellendus facilis eum quos aliquid commodi consequatur
+          quae, saepe iure quia labore ipsum dolore.
+        </h3>
+      </header>
+      <main>
+        <section className="input-container">
+          <input
+            className="email-input"
+            type="text"
+            name="email-input"
+            id="email-input"
+            placeholder="Enter your email to update"
+          />
+          <button className="submit-button" type="button">
+            Submit
+          </button>
+        </section>
+        <section className="support-icons-container">
+          <img className="support-icons-img" src={ FacebookIcon } alt="Facebook icon" />
+          <img className="support-icons-img" src={ TwitterIcon } alt="Twitter icon" />
+          <img className="support-icons-img" src={ GoogleIcon } alt="Google icon" />
+          <img className="support-icons-img" src={ PinterestIcon } alt="Pinterest icon" />
+        </section>
+      </main>
+      <footer>
+        <SupportFooter />
+      </footer>
     </div>
-  )
+  ) 
 }
